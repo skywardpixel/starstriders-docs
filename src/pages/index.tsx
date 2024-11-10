c9a08052c3355f72a9f08f67422c098985a44789
Kyle Yan <kyleyankc@gmail.com>
2024-11-10 00:16:09 -0800

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,36 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
-import Translate from '@docusaurus/Translate';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/rules">
-            <Translate>View Rules</Translate>
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import styles from "./index.module.css";
+import Translate from "@docusaurus/Translate";
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        {/* <HomepageFeatures /> */}
-      </main>
+      description="Description will go into a meta tag in <head />"
+    >
+      <div className={styles.heroContainer}>
+        <main>
+          <Heading as="h1" className={styles.heroTitle}>
+            Starstriders
+          </Heading>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/rules"
+            >
+              <Translate>View Rules</Translate>
+            </Link>
+          </div>
+        </main>
+      </div>
     </Layout>
   );
 }
